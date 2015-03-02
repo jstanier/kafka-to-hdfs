68251d7601fb13cc2565d5c6e63e236f93c9c7c8
James Stanier <jamess@brandwatch.com>
2015-03-02 06:48:11 +0000

    Handle exceptions in StreamConsumer. Fixes #8

--- src/main/java/com/jstanier/hdfswriter/StreamConsumer.java
+++ src/main/java/com/jstanier/hdfswriter/StreamConsumer.java
@@ -2,6 +2,8 @@ package com.jstanier.hdfswriter;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,8 @@ import com.brandwatch.kafka.consumer.StreamIterator;
 @Component
 @Scope("prototype")
 final class StreamConsumer implements Runnable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StreamConsumer.class);
 
     @Autowired
     private StreamIterator<String, String> streamIterator;
@@ -23,6 +27,10 @@ final class StreamConsumer implements Runnable {
             while (streamIterator.hasNext()) {
                 hdfsWriter.write(streamIterator.next().message());
             }
-        } catch (InterruptedException e) {} catch (IOException e) {}
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (IOException e) {
+            LOG.error("IOException when writing to HDFS.", e);
+        }
     }
 }
